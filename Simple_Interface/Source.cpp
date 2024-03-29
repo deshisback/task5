496eae2f2cd75646b2dd536033f4c3b1488f7e68
Gleb <Aut1Matik@yandex.ru>
2024-03-29 21:15:56 +1000

    added and implemented the Student class

--- Simple_Interface/Source.cpp
+++ Simple_Interface/Source.cpp
@@ -23,11 +23,11 @@ private:
 
 public:
 
-	Human(int& _Age, string& _Name, int& _Heigh)
+	Human(int _Age, string _Name, int _Heigh)
 	{
-		Age = _Age;
-		Name = _Name;
-		Heigh = _Heigh;
+		SetAge(_Age);
+		SetName(_Name);
+		SetHeigh(_Heigh);
 	}
 
 	int GetAge()
@@ -45,7 +45,7 @@ public:
 		return Heigh;
 	}
 
-	void SetAge(int& _Age)
+	void SetAge(int _Age)
 	{
 		if (_Age < 0)
 		{
@@ -55,12 +55,12 @@ public:
 		Age = _Age;
 	}
 
-	void SetName(string& _Name)
+	void SetName(string _Name)
 	{
 		Name = _Name;
 	}
 
-	void SetHeigh(int& _Heigh)
+	void SetHeigh(int _Heigh)
 	{
 		if (_Heigh <= 150)
 		{
@@ -80,6 +80,45 @@ public:
 	}
 };
 
-int main() {
+class Student : public Human
+{
+private:
+	int Course;
+	string University;
+public:
+
+	Student(int _Age, string _Name, int _Heigh, int _Course, string _University) : Human(_Age, _Name, _Heigh) 
+	{
+		Course = _Course;
+		University = _University;
+		cout << "Deadline gorit? Da i bes s nim, u menya budet vtoraya komissiya, ya sdam vse dolgo vovremya!";
+	}
 	
+	void SetCourse(int _Course)
+	{
+		Course = _Course;
+	}
+
+	void SetUniversity(string _University)
+	{
+		University = _University;
+	}
+
+	int GetCouse()
+	{
+		return Course;
+	}
+
+	string GetUniversity()
+	{
+		return University;
+	}
+};
+
+int main() {
+	string name = "Yakov";
+	string uni = "FEFU";
+	Human human(15, name, 163);
+	Student s(15, name, 163, 3, uni);
+	cout << s.GetAge();
 }
